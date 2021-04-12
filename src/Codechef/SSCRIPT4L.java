e716ba3cf39f91fcf59b02b5938823582f6c511c
m-s-n <msn2106@gmail.com>
2021-04-12 18:50:26 +0530

    SSCRIPT4L

--- src/Codechef/SSCRIPT4L.java
+++ src/Codechef/SSCRIPT4L.java
@@ -7,32 +7,36 @@ public class SSCRIPT4L {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextInt()){
             int t = sc.nextInt();
-            while(t-- >0){
+            while(t-- >0) {
                 int n = sc.nextInt();
                 int k = sc.nextInt();
                 sc.nextLine();
                 String s = sc.nextLine();
+                char[] ch = s.toCharArray();
+                int[] arr = new int[ch.length];
 
-                //solution starts here
-                long startTime = System.currentTimeMillis();
-                //  char[] ch = new char[k];
-                //  Arrays.fill(ch,"*");
-                //  String star = new String(ch);
-                //  String star = new String(new char[k]).replace('\0', '*');
-                StringBuilder str = new StringBuilder();
-                StringBuilder star = new StringBuilder("*");
-                for(int i=0;i<k;i++){
-                    str = str.append(star);
+                for(int i=0;i<ch.length;i++){
+                    if(ch[i]=='*'){
+                        ch[i] = '1';
+                    }
+                    else ch[i]='0';
                 }
 
-                if(str.length() < star.length()){
-                    System.out.println("NO");
-                    return;
-                }
-                for(int i=0;i<str.length()-k;i++){
-
+                arr[0] = Integer.parseInt(String.valueOf(ch[0]));
+                for(int i=1;i<n;i++){
+                    arr[i] = arr[i-1]+ Integer.parseInt(String.valueOf(ch[i]));
                 }
 
+                int sum = 0,flag = 0;
+                for(int i=0; i+k-1 < n; i++){
+                    sum = arr[i+k-1] - ((i == 0 ? 0 : arr[i-1]));
+                    if(sum >= k){
+                        System.out.println("YES");
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0)System.out.println("NO");
             }
         }
         sc.close();
@@ -47,3 +51,4 @@ public class SSCRIPT4L {
 5 1
 abcde
  */
+//Watched it from CodeChef video tutorials

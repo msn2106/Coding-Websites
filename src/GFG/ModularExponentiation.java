7ccea59beadfff2f08d8fd370ff0bd2279124eba
msn2106 <msn2106@gmail.com>
2021-05-12 08:19:21 +0530

    ModularExponentiationGFG_Mod

--- src/GFG/ModularExponentiation.java
+++ src/GFG/ModularExponentiation.java
@@ -6,26 +6,24 @@ public class ModularExponentiation
 {
 
     /* Iterative Function to calculate (x^y) in O(log y) */
-    static long power(int x, int y, int p)
+    static long power(long x, long y, long p)
     {
         long res = 1; // Initialize result
 
-        x = x % p; // Update x if it is more than or
-        // equal to p
+        x = x % p; // Update x if it is more than or equal to p
 
         if (x == 0)
             return 0; // In case x is divisible by p;
 
         while (y > 0)
         {
-
             // If y is odd, multiply x with result
             if ((y & 1) != 0)
-                res = (res * x) % p;
+                res = (res * (x%p)) % p;
 
             // y must be even now
             y = y >> 1; // y = y/2
-            x = (x * x) % p;
+            x = ((x%p) * (x%p)) % p;
         }
         return res;
     }
@@ -33,13 +31,11 @@ public class ModularExponentiation
     // Driver Code
     public static void main(String[] args)
     {
-        int x = 2;
-        int y = 100000;
-        int p = (int)Math.pow(10,9)+7;
-        System.out.print("Power is " + power(x, y, p));
+        long base = 2;
+        long power = 100000;
+        //long power = 34;
+        System.out.print("Power is " + power(base, power-1, 1000000007));
     }
 }
 
 // Copied from GFG - Similar Question May Long Challenge XOREQUAL
-//In CPP same code is working for any no. but in Java it is not.
-//The limit for y should be 100000.
